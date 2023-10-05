ed28bcb31353f6de431438084936ccd698077351
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2023-10-05 20:02:45 +0800

    -initial setup and router setup

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# WHENEVER YOU PUSH TO THE STAGIN BRANCH, MAKE SURE THAT YOU ADD THE CHANGES 
+
+# ver 0.0.1 Setup environment
+
+# ver 0.0.2 install packages
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
+ 
+ <router-view></router-view>
+ 
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+ </template>
+ 
+ <script>
+ import LoginForm from './views/LoginForm'
+ import Dashboard from './views/Dashboard'
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+ export default {
+   name: 'App',
+   components: {
+     LoginForm,
+     Dashboard
+   }
+ }
+ </script>
+ 
+ <style>
+ #app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
+ }
+ </style>
+ 
