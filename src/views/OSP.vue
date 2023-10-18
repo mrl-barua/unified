6794fe1f2389c96cfd68d204b6a95aa800c8d2dc
M4RAX <suertemara@gmail.com>
2023-10-18 10:31:07 +0800

    version 1.2

--- src/views/OSP.vue
+++ src/views/OSP.vue
@@ -4,62 +4,98 @@
 
     <br><br><br><br>
 
-    <h1>OPERATIONAL STAFF DASHBOARD</h1>
-
+    <div class="col-12 col-md-6">
+      <div class="dashboard-box">
+        <div style="background-color: red; color: white; padding: 10px;">
+          EMPLOYMENT
+        </div>
+        <div style="display: flex;">
+          <div style="background-color: ; color: black; flex: 1; padding: 10px;">
+            IP GROUP
+          </div>
+          <div style="background-color: ; color: black; flex: 1; padding: 10px;">
+            PWD
+          </div>
+          <div style="background-color: ; color: black; flex: 1; padding: 10px;">
+            SOLO PARENT
+          </div>
+        </div>
+        <div>
+          0&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0
+        </div>
+        <!-- More content here -->
+      </div>
+    </div>
 
     <div class="col-12 col-md-6">
-          <DougnutChart :data="NameData" />
+      <div class="dashboard-box">
+        <div style="background-color: red; color: white; padding: 10px;">
+          DIVISION
         </div>
+        <div style="background-color: ; color: black; padding: 10px;">
+          RECORD COUNT
+        </div>
+        <div>
+          <div>
+            Division #1 
+          </div>
+          <div>
+            Division #2 
+          </div>
+          <div>
+            Division #3 
+          </div>
+          <div>
+            Division #4 
+          </div>
+        </div>
+      </div>
+    </div>
 
-        <div class="col-12 col-md-6">
-          <DougnutChart :data="MonthData" />
-        </div></div>
-    <!-- <Footer /> -->
+    <div class="col-12 col-md-6">
+      <DougnutChart :data="NameData" />
+    </div>
+
+    <div class="col-12 col-md-6">
+      <DougnutChart :data="MonthData" />
+    </div>
+  </div>
 </template>
 
 <script>
 import Sidebar from '@/components/Sidebar.vue'; 
 import Footer from '@/components/Footer';
 
-  import BarChart from '@/components/ChartJS/Barchart';
-  import PieChart from '@/components/ChartJS/PieChart';
-  import LineChart from '@/components/ChartJS/LineChart';
-  import DougnutChart from '@/components/ChartJS/DoughnutChart';
-  import PolarAreaChart from '@/components/ChartJS/PolarArea';
-  import RadarChart from '@/components/ChartJS/Radar';
-export default {
-    name: 'OSP',
-    components: {
-        Sidebar,
-        Footer,
-        BarChart,
-        PieChart,
-        LineChart,
-        DougnutChart,
-        PolarAreaChart,
-        RadarChart,
-    },
+import DougnutChart from '@/components/ChartJS/DoughnutChart';
 
-    data() {
-      return {
-        MonthData: {
-          labels: ['WEDC', 'OTHERS', 'FHONA', 'PWD', 'OLDER PERSONS', 'OFW'],
-          label: ['CHART1'],
-          values: [39, 18, 13, 4, 3, 1],
-          backgroundColor: [
+export default {
+  name: 'OSP',
+  components: {
+    Sidebar,
+    Footer,
+    DougnutChart,
+  },
+
+  data() {
+    return {
+      MonthData: {
+        labels: ['WEDC', 'OTHERS', 'FHONA', 'PWD', 'OLDER PERSONS', 'OFW'],
+        label: ['CHART1'],
+        values: [39, 18, 13, 4, 3, 1],
+        backgroundColor: [
           'rgba(25, 82, 105, 0.6',  
           'rgba(85, 25, 44, 0.6',  
           'rgba(135, 146, 56, 0.6',
           'rgba(25, 61, 95, 0.6', 
           'rgba(105, 82, 105, 0.6', 
         ],
-        },
-        
-        NameData: {
-          labels: ['CNSP', 'EMOTIONALLY/PSYCHO DISTRESS', 'OFW'],
-          label: ['CHART2'],
-          values: [42, 23, 12, 2],
-          backgroundColor: [
+      },
+      
+      NameData: {
+        labels: ['CNSP', 'EMOTIONALLY/PSYCHO DISTRESS', 'OFW'],
+        label: ['CHART2'],
+        values: [42, 23, 12, 2],
+        backgroundColor: [
           'rgba(75, 192, 192, 0.2',
           'rgba(255, 99, 132, 0.2',
           'rgba(255, 206, 86, 0.2',
@@ -67,11 +103,9 @@ export default {
           'rgba(153, 102, 255, 0.2',
           'rgba(255, 159, 64, 0.2',
         ],
-        },
-
-      };
-    },
-    
+      },
+    };
+  },
 }
 </script>
 
