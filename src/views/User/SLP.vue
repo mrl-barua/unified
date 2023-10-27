283a05f372d4287c878c044f387dbbfac5efbc2c
pondekram <mjbpondol@usep.edu.ph>
2023-10-27 23:38:22 +0800

    SLP - GDA Partially done - Mark

--- src/views/User/SLP.vue
+++ src/views/User/SLP.vue
@@ -17,17 +17,15 @@
     <div class="col-12 col-md-6">
         <div class="shadow2 forbarchart">
            <p><b>REFERRALS</b></p>
-          <div class="Barchart1"><HBarChart :data="SLPData" /></div>
+          <div class="Barchart1"><HBarChart :data="RefData" /></div>
         </div>
     </div>
 
 <!-- 3 -->
     <div class="col-12 col-md-6">
-      <div class="shadow2">
-           <p><b>SECTORAL ACCOMPLISHMENT</b></p>
-          <div class="Piechart1">
-            <DoughnutChart v-if="EmploymentData" :data="EmploymentData" />
-          </div>
+        <div class="shadow2">
+          <p><b>SECTORAL ACCOMPLISHMENT</b></p>
+          <div class="DoughnutChart"><DoughnutChart :data="SecData" /></div>
         </div>
     </div>
 
@@ -35,14 +33,9 @@
     <div class="col-12 col-md-6">
         <div class="shadow2 forbarchart">
            <p><b>BREAKDOWN OF REGIONAL ACCOMPLISHMENT</b></p>
-          <div class="Barchart1"><HBarChart :data="SLPData" /></div>
+          <div class="Barchart1"><BarChart :data="ProvinceData" /></div>
         </div>
     </div>
-
- <!-- Can be use as reference for piechart -->
-   
-
-
 
 
     
@@ -57,146 +50,72 @@ import Footer from '@/components/Footer';
 import BarChart from '@/components/ChartJS/Barchart';
 import PieChart from '@/components/ChartJS/PieChart';
 import DoughnutChart from '@/components/ChartJS/DoughnutChart';
+import HBarChart from '@/components/ChartJS/HBarchart';
 
 
 export default {
-    name: 'HR',
+    name: 'SLP',
     components: {
         Sidebar,
         Footer,
         BarChart,
         PieChart,
+        HBarChart,
         DoughnutChart
+        
     
     },
 
     data() {
       return {
-
-         MonthData: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November' , 'December'],
-          label: ['Months'],
-          values: [39, 18, 13, 4, 3, 1, 2, 3, 4, 5, 4, 2],
-          backgroundColor: [
-          'rgba(19, 63, 92, 1)'
-        ],
-        },
-
         SLPData: {
-          labels: ['Jan', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November' , 'December'],
-          label: ['Months'],
-          values: [39, 18, 13, 4, 3, 1, 2, 3, 4, 5, 4, 2],
-          backgroundColor: [
-          'rgba(19, 63, 92, 1)'
-        ],
-        },   
+          labels: ['EO 70', 'CVA', 'SLP', 'REGULAR', 'ZERO', 'HUNGER', 'EO 70', 'FRs', 'REFERRAL'],
+          label: ['PHYSICAL TARGET']['TOTAL'],
+        
 
-        NameData: {
-          labels: ['CNSP', 'EMOTIONALLY/PSYCHO DISTRESS', 'OFW'],
-          label: ['CHART2'],
-          values: [42, 23, 12, 2],
+          values: [25, 18, 13, 4, 3, 1, 2, 3, 4, 5, 4, 2],
           backgroundColor: [
-          'rgba(75, 192, 192, 0.2',
-          'rgba(255, 99, 132, 0.2',
-          'rgba(255, 206, 86, 0.2',
-          'rgba(54, 162, 235, 0.2',
-          'rgba(153, 102, 255, 0.2',
-          'rgba(255, 159, 64, 0.2',
+          '#133F5C', '#F11B25'
+        ],
+        },  
+        RefData: {
+          labels: ['REFERRAL BY PARTNERS', ' ', 'WALK IN', 'OBSUs', ' ', 'THRU EMAIL', ' ', 'NPMO', 'ENDORSEMENT',],
+          label: ['YEAR 2022'],
+          values: [0,0, 18, 13, 4, 3, 1, 20, 40, 4, 5, 4, 2],
+          backgroundColor: [
+          '#F11B25', '#133F5C'
+        ],
+        },    
+
+        SecData: {
+          labels: ['IP', 'SENIOR CITIZENS', 'SOLO PARENT', 'PWD', 'LGBTQ', 'YOUTH'],
+          label: ['CHART2'],
+          values: [10, 23, 15,27,20,24],
+          backgroundColor: [
+          '#59508D',
+          '#BC5090',
+          '#EB5F5E',
+          '#F3A533',
+          '#133F5C',
+          '#252525',
         ],
         },
 
 
-        EmploymentData: null,
+       ProvinceData: {
+          labels: ['DAVAO ORIENTAL', 'DAVAO DEL SUR', 'DAVAO CITY', 'DAVAO DE ORO', 'DAVAO DEL NORTE', 'DAVAO OCCIDENTAL'],
+          label: ['ACCOMPLISHMENT'],
+          values: [120, 100, 400, 150, 250, 200],
+          backgroundColor: [
+          'rgba(19, 63, 92, 1)'
+        ],
+        },    
+
       };
     },
 
  methods:{
-  EmploymentFetchData() {
-      return axios
-        .get('http://127.0.0.1:8000/api/employmentStatus')
-        .then(response => {
-          // Initialize data arrays
-          const moa = [];
-          const permanent = [];
-          const contractual = [];
-          const coterminos = [];
-          const casual = [];
-         
-
-          response.data.forEach(item => {
-            const employmentstatus = item.EMPLOYMENT_STATUS;
-
-            switch (employmentstatus) {
-              case 'MOA':
-                  moa.push(item);
-                break;
-              case 'PERMANENT':
-                  permanent.push(item);
-                break;
-              case 'CONTRACTUAL':
-                 contractual.push(item);
-                break; 
-              case 'COTERMINOS':
-                coterminos.push(item);
-                break; 
-              case 'CASUAL':
-                casual.push(item);
-                break; 
-              default:
-                // Handle other cases if necessary
-                break;
-            }
-          });
-
-          // Calculate data lengths
-          const moaLength = moa.length;
-          const permanentLength = permanent.length;
-          const contractualLength = contractual.length;
-          const coterminosLength = coterminos.length;
-          const casualLength = casual.length;
-          // Prepare and return data
-          const employmentdata = {
-            labels: ['MOA', 'PERMANENT', 'CONTRACTUAL', 'COTERMINOS', 'CASUAL'],
-            label: ['Employment Data'],
-            values: [moaLength, permanentLength, contractualLength, coterminosLength, casualLength],
-            backgroundColor: ['rgba(19, 63, 92, 1)',
-                              'rgba(243, 165, 51, 1)',
-                              'rgba(235, 95, 94, 1)',
-                              'rgba(0, 255, 0, 0.6)',
-                              'rgba(0, 0, 255, 0.6)',
-                            
-          ],
-          };
-          // Set barChartData to the computed data
-          this.EmploymentData = employmentdata;
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-
-
-          const employmentdata = {
-            labels: ['MOA', 'PERMANENT', 'CONTRACTUAL', 'COTERMINOS', 'CASUAL'],
-            label: ['Employment Data'],
-            values: [1, 1, 1, 1, 1],
-            backgroundColor: ['rgba(25, 82, 105, 0.6)',
-                              'rgba(0, 255, 0, 0.6)',
-                              'rgba(0, 0, 255, 0.6)',
-                            
-          ],
-          };
-          // Set barChartData to the computed data
-          this.EmploymentData = employmentdata;
-
-        });
-    },
-
- },
-
-
-
-  mounted() {
-    // Automatically fetch data when the component is mounted
-    this.EmploymentFetchData();
+//   
   },
 
     
@@ -240,3 +159,4 @@ export default {
 }
 
 </style>
+
