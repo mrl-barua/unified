213d9bb43f6e14aa53f0427a4b262762d92faa08
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2023-11-08 21:13:47 +0800

    Test datatable , Dont merge yet

--- src/main.js
+++ src/main.js
@@ -5,4 +5,13 @@ import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle'; // Import Bootstrap JavaScript
 import { Chart } from 'vue-chartjs'
 
+import DataTable from "datatables.net-vue3";
+import DataTablesCore from "datatables.net";
+import "datatables.net-responsive";
+
+DataTable.use(DataTablesCore);
+
+
+
+
 createApp(App).use(router).mount('#app')
