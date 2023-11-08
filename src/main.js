ee76c0a272ca9c00b2803568ee1d734e15fc7a27
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2023-11-08 22:12:00 +0800

    test 2

--- src/main.js
+++ src/main.js
@@ -5,11 +5,9 @@ import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle'; // Import Bootstrap JavaScript
 import { Chart } from 'vue-chartjs'
 
-import DataTable from "datatables.net-vue3";
-import DataTablesCore from "datatables.net";
-import "datatables.net-responsive";
 
-DataTable.use(DataTablesCore);
+
+
 
 
 
