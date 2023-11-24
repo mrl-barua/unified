1ea807b8ffe42f6d8335f95e7b9553e8ff419703
pondekram <mjbpondol@usep.edu.ph>
2023-11-24 16:51:09 +0800

    SLP REVAMP DONE  GDD - MARK

--- src/views/User/SLP/SLP.vue
+++ src/views/User/SLP/SLP.vue
@@ -3,109 +3,127 @@
 
   <br /><br /><br /><br />
 
-  <h1><b>TOTAL ACCOMPLISHMENT VS PHYSICAL TARGET</b></h1>
-
   <!-- 1 -->
-  <div class="row">
-    <div class="col-12 col-md-8">
-      <div class="shadow2 forbarchart">
-        <div class="Barchart1"><HBarChart :data="SLPData" /></div>
-      </div>
+  <div class="col-12 col-md-8">
+    <div class="shadow2 forbarchart">
+      <h1>TOTAL ACCOMPLISHMENT VS PHYSICAL TARGET</h1>
+      <div class="Barchart1"><HBarChart :data="SLPData" /></div>
     </div>
-    <div class="col-12 col-md-4">
-      <div class="rectangle" style="background-color: #133f5c">
-        <div class="label">PHYSICAL TARGET</div>
-        <div class="number"><b>6,521</b></div>
-      </div>
-      <div class="rectangle" style="background-color: #f11b25">
-        <div class="label">TOTAL</div>
-        <div class="number"><b>956</b></div>
-      </div>
-      <div class="rectangle" style="background-color: #f3a533">
-        <div class="label">REMAINING</div>
-        <div class="number"><b>5,565</b></div>
-      </div>
+  </div>
+  <div class="col-12 col-md-4">
+    <div
+      class="rectangle"
+      style="
+        background: #4e91fd;
+        box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.15);
+      "
+    >
+      <div class="label">PHYSICAL TARGET</div>
+      <div class="number"><b>6,521</b></div>
+    </div>
+    <div
+      class="rectangle"
+      style="
+        background: #ff4040;
+        box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.15);
+      "
+    >
+      <div class="label">TOTAL</div>
+      <div class="number"><b>956</b></div>
+    </div>
+    <div
+      class="rectangle"
+      style="
+        background: #eeb600;
+        box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.15);
+      "
+    >
+      <div class="label">REMAINING</div>
+      <div class="number"><b>5,565</b></div>
     </div>
   </div>
 
   <!-- 2 -->
-  <h1><b> REFERRALS</b></h1>
+
   <div class="row">
-    <div class="col-12 col-md-8">
-      <div class="shadow3 forbarchart">
-        <div class="Barchart1"><HBarChart :data="RefData" /></div>
+    <div class="col-12 col-md-4">
+      <div class="parent">
+        <div class="child">
+          <div class="rectangle1" style="background: #eeb600">
+            <div class="label">PHYSICAL TARGET</div>
+            <div class="number"><b>20</b></div>
+          </div>
+          <div class="rectangle1" style="background-color: #ff4040">
+            <div class="label">YEAR 2022</div>
+            <div class="number"><b>13</b></div>
+          </div>
+          <div class="rectangle1" style="background-color: #4e91fd">
+            <div class="label">YEAR 2023</div>
+            <div class="number"><b>12</b></div>
+          </div>
+        </div>
+        <div class="child">
+          <div class="rectangle1" style="background-color: #133f5c">
+            <div class="label">QUALIFIED / SERVED</div>
+            <div class="number"><b>13</b></div>
+          </div>
+          <div class="rectangle1" style="background-color: #133f5c">
+            <div class="label">FUNDED</div>
+            <div class="number"><b>8</b></div>
+          </div>
+          <div class="rectangle1" style="background-color: #133f5c">
+            <div class="label">ENDORSED TO PARTNERS</div>
+            <div class="number"><b>0</b></div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="col-12 col-md-4">
-      <div class="rectangle1" style="background-color: #f3a533">
-        <div class="label">PHYSICAL TARGET</div>
-        <div class="number"><b>20</b></div>
-      </div>
-      <div class="rectangle1" style="background-color: #f11b25">
-        <div class="label">YEAR 2022</div>
-        <div class="number"><b>13</b></div>
-      </div>
-      <div class="rectangle1" style="background-color: #133f5c">
-        <div class="label">YEAR 2023</div>
-        <div class="number"><b>12</b></div>
-      </div>
-      <div class="rectangle1" style="background-color: #252525">
-        <div class="label">QUALIFIED / SERVED</div>
-        <div class="number"><b>13</b></div>
-      </div>
-      <div class="rectangle1" style="background-color: #252525">
-        <div class="label">FUNDED</div>
-        <div class="number"><b>8</b></div>
-      </div>
-      <div class="rectangle1" style="background-color: #252525">
-        <div class="label">ENDORSED TO PARTNERS</div>
-        <div class="number"><b>0</b></div>
+    <div class="col-12 col-md-8">
+      <div class="shadow2 forbarchart">
+        <h1>REFERRALS</h1>
+        <div class="Barchart1"><HBarChart :data="RefData" /></div>
       </div>
     </div>
   </div>
 
   <!-- 3 -->
-  <br />
-  <h1><b>SECTORAL ACCOMPLISHMENT</b></h1>
-  <br />
-  <div class="row">
-    <div class="col-12 col-md-4">
-      <div class="rectangle4">
-        <div class="label">DISAGGREGATED</div>
-        <div class="label1"><b>DATA</b></div>
-      </div>
 
-      <div class="rectangle" style="background-color: #59508d">
-        <div class="label">MALE</div>
-        <div class="number"><b>267</b></div>
-      </div>
-      <div class="rectangle" style="background-color: #bc5090">
-        <div class="label">FEMALE</div>
-        <div class="number"><b>589</b></div>
-      </div>
-      <div class="rectangle" style="background-color: #f3a533">
-        <div class="label">TOTAL</div>
-        <div class="number"><b>856</b></div>
-      </div>
+  <div class="col-12 col-md-5">
+    <div class="shadow2 forbarchart2">
+      <h1>BREAKDOWN OF REGIONAL ACCOMPLISHMENT</h1>
+      <div class="Barchart2"><BarChart :data="ProvinceData" /></div>
     </div>
+  </div>
 
-    <div class="col-12 col-md-8">
+  <!-- 4 -->
+
+  <div class="container">
+    <div class="col-12 col-md-6">
       <div class="shadow5">
+        <h1>SECTORAL ACCOMPLISHMENT</h1>
         <div class="DoughnutChart" style="height: 370px">
           <DoughnutChart :data="SecData" />
         </div>
       </div>
     </div>
   </div>
-  <!-- 4 -->
 
-  <br />
-  <h1><b>BREAKDOWN OF REGIONAL ACCOMPLISHMENT</b></h1>
-  <br />
-  <div class="col-12 col-md-12">
-    <div class="shadow2 forbarchart">
-      <div class="Barchart1"><BarChart :data="ProvinceData" /></div>
-    </div>
+  <div class="rectangle4">
+    <div class="label">DISAGGREGATED</div>
+    <div class="label1"><b>DATA</b></div>
+  </div>
+
+  <div class="rectangle3" style="background-color: #4e91fd">
+    <div class="label">MALE</div>
+    <div class="number"><b>267</b></div>
+  </div>
+  <div class="rectangle3" style="background-color: #ff4040">
+    <div class="label">FEMALE</div>
+    <div class="number"><b>589</b></div>
+  </div>
+  <div class="rectangle3" style="background-color: #133f5c">
+    <div class="label">TOTAL</div>
+    <div class="number"><b>856</b></div>
   </div>
 
   <!-- <Footer /> -->
@@ -150,7 +168,7 @@ export default {
         ],
         label: ["PHYSICAL TARGET"]["TOTAL"],
         values: [25, 18, 13, 4, 3, 1, 2, 3, 4, 5, 4, 2],
-        backgroundColor: ["#133F5C", "#F11B25"],
+        backgroundColor: ["#4E91FD", "#FF4040"],
       },
       RefData: {
         labels: [
@@ -166,7 +184,7 @@ export default {
         ],
         label: ["YEAR 2022"],
         values: [0, 0, 18, 13, 4, 3, 1, 20, 40, 4, 5, 4, 2],
-        backgroundColor: ["#F11B25", "#133F5C"],
+        backgroundColor: ["#FF4040", "#4E91FD"],
       },
 
       SecData: {
@@ -181,12 +199,12 @@ export default {
         label: ["CHART2"],
         values: [10, 23, 15, 27, 20, 24],
         backgroundColor: [
-          "#59508D",
-          "#BC5090",
-          "#EB5F5E",
-          "#F3A533",
-          "#133F5C",
-          "#252525",
+          "#B2DAFA",
+          "#84B6F4",
+          "#F8E44B",
+          "#EECA06",
+          "#FF9688",
+          "#FF6961",
         ],
       },
 
@@ -201,7 +219,7 @@ export default {
         ],
         label: ["ACCOMPLISHMENT"],
         values: [120, 100, 400, 150, 250, 200],
-        backgroundColor: ["#59508D"],
+        backgroundColor: ["#EEB600"],
       },
     };
   },
@@ -214,15 +232,31 @@ export default {
 
 <style scoped>
 .shadow2 {
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  height: 400px;
-  border-radius: 20px;
-  margin: 10px 15px 10px 20px;
-  padding: 10px 0px 10px 0px;
+  color: #252525;
+  text-align: center;
+  font-family: Arial;
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  background: #fff;
+  box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.15);
+  margin: 0.5rem;
+}
+.shadow2 h1 {
+  color: #252525;
+  text-align: center;
+  font-family: Arial;
+  font-size: 27px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  padding-top: 1.5rem;
 }
 
 .forbarchart {
   height: 250px;
+
   @media only screen and (min-width: 500px) {
     height: 350px;
   }
@@ -230,9 +264,12 @@ export default {
     height: 400px;
   }
 }
-
+/* this refers to the gap between text and graph */
 .Barchart1 {
-  height: 340px;
+  height: 280px;
+}
+.Barchart2 {
+  height: 332px;
 }
 
 /* CSS FOR DATA SUMMARY */
@@ -256,21 +293,8 @@ export default {
 
 /* css */
 
-.stacked-rectangle {
-  display: flex;
-  justify-content: space-between;
-}
-
-.stacked-rectangle .col-12.col-md-4 {
-  width: 20%;
-}
-
-.stacked-rectangle .shadow2 {
-  background: transparent;
-}
-
 .rectangle {
-  padding: 10px;
+  padding: 22px;
   margin: 10px;
   color: white;
   display: flex;
@@ -294,27 +318,28 @@ export default {
   text-align: center;
 }
 
-.shadow3 {
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  height: 495px;
-  border-radius: 20px;
-  margin: 50px 15px 10px 20px;
-  padding: 10px 0px 10px 0px;
+.shadow5 {
+  color: #252525;
+  text-align: center;
+  font-family: Arial;
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  background: #fff;
+  box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.15);
+  margin: 0.5rem;
 }
 
-.shadow4 {
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  height: 550px;
-  border-radius: 20px;
-  margin: 50px 15px 10px 20px;
-  padding: 10px 0px 10px 0px;
-}
-.shadow5 {
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  height: 440px;
-  border-radius: 20px;
-  margin: 20px 15px 10px 20px;
-  padding: 10px 0px 10px 0px;
+.shadow5 h1 {
+  color: #252525;
+  text-align: center;
+  font-family: Arial;
+  font-size: 27px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  padding-top: 1.5rem;
 }
 
 .rectangle4 {
@@ -331,7 +356,7 @@ export default {
 .rectangle4 .label {
   color: #133f5c;
   text-align: center;
-  font-size: 35px;
+  font-size: 22px;
   font-style: bold;
   font-weight: 700;
   line-height: normal;
@@ -357,28 +382,15 @@ export default {
 
 /* referrals */
 
-.stacked-rectangle1 {
-  display: flex;
-  justify-content: space-between;
-}
-
-.stacked-rectangle1 .col-12.col-md-4 {
-  width: 20%;
-}
-
-.stacked-rectangle1 .shadow2 {
-  background: transparent;
-}
-
 .rectangle1 {
-  padding: 5px;
-  margin: 10px;
+  margin: 2px;
   color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 20%;
+  width: 212px;
+  height: 134px;
 }
 
 .rectangle1 .label {
@@ -393,5 +405,37 @@ export default {
 .rectangle1 .number {
   font-size: 35px;
   text-align: center;
+}
+
+/* css for rec 3 */
+
+.rectangle3 {
+  padding: 12px;
+  margin: 10px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 20%;
+}
+
+.rectangle3 .label {
+  color: #fff;
+  text-align: center;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+}
+
+.rectangle3 .number {
+  font-size: 40px;
+  text-align: center;
+}
+
+.parent {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
 }
 </style>
