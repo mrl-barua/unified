0d30c55b6cc382ab8f7888bf7b1b2e7523eed702
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2024-01-02 22:44:38 +0800

    Update backendURL in config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 // config.js
 
 // If you want to use your own backend, change this to the URL of your Strapi backend.
-export const backendURL = 'http://127.0.0.1:8000'; // THIS IS FOR ARTISAN SERVE USING LARAVEL
-// export const backendURL = 'http://local.unifast'; // THIS IS FOR VHOSTS ON LOCALHOST
-
+// export const backendURL = 'http://127.0.0.1:8000'; // THIS IS FOR ARTISAN SERVE USING LARAVEL
+export const backendURL = 'http://local.unifast'; // THIS IS FOR VHOSTS ON LOCALHOST
